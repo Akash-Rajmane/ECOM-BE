ad916c1c8e643b5f5433eef0fa5abbe49c9b66eb
Akash <akashrajmane23@gmail.com>
2024-11-16 01:58:04 +0530

    optimized getCart & getProducts

--- controllers/product.js
+++ controllers/product.js
@@ -1,32 +1,31 @@
-const Product = require('../models/product');
+const Product = require("../models/product");
 
 const getProducts = async (req, res) => {
-    try {
-        const products = await Product.find();
-        //console.log(products)
-        res.status(200).json(products);
-    } catch (err) {
-        res.status(500).json({ error : 'Internal Server Error' });
-    }
+  try {
+    const products = await Product.find().lean();
+    //console.log(products)
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 // Admin level
 const createProduct = async (req, res) => {
-    const { name, description, category, price, images } = req.body;
-    try {
-        const newProduct = new Product({
-            name,
-            description,
-            category,
-            price,
-            images
-        });
-        const product = await newProduct.save();
-        res.status(201).json(product);
-    } catch (err) {
-        res.status(500).json({ error : 'Internal Server Error' });
-    }
+  const { name, description, category, price, images } = req.body;
+  try {
+    const newProduct = new Product({
+      name,
+      description,
+      category,
+      price,
+      images,
+    });
+    const product = await newProduct.save();
+    res.status(201).json(product);
+  } catch (err) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
-
 
 module.exports = { getProducts, createProduct };
